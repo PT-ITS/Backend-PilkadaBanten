3916f74ca9c5102f7bdb5f585522b3376afb0919
Rafli Januar Rizki <raflijr3101@gmail.com>
2024-10-10 16:23:37 +0700

    delete every data from sql except banten data

--- sql/master_provinsi.sql
+++ sql/master_provinsi.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Sep 18, 2022 at 04:45 AM
--- Server version: 8.0.30-0ubuntu0.20.04.2
--- PHP Version: 7.4.3
+-- Host: 127.0.0.1
+-- Generation Time: Oct 10, 2024 at 11:19 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `master_data`
+-- Database: `backend_pilkada_banten`
 --
 
 -- --------------------------------------------------------
@@ -29,49 +28,16 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `master_provinsi` (
-  `id` char(2) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
-  `name` tinytext CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+  `id` char(2) NOT NULL,
+  `name` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `master_provinsi`
 --
 
 INSERT INTO `master_provinsi` (`id`, `name`) VALUES
-('11', 'ACEH'),
-('12', 'SUMATERA UTARA'),
-('13', 'SUMATERA BARAT'),
-('14', 'RIAU'),
-('15', 'JAMBI'),
-('16', 'SUMATERA SELATAN'),
-('17', 'BENGKULU'),
-('18', 'LAMPUNG'),
-('19', 'KEPULAUAN BANGKA BELITUNG'),
-('21', 'KEPULAUAN RIAU'),
-('31', 'DKI JAKARTA'),
-('32', 'JAWA BARAT'),
-('33', 'JAWA TENGAH'),
-('34', 'DI YOGYAKARTA'),
-('35', 'JAWA TIMUR'),
-('36', 'BANTEN'),
-('51', 'BALI'),
-('52', 'NUSA TENGGARA BARAT'),
-('53', 'NUSA TENGGARA TIMUR'),
-('61', 'KALIMANTAN BARAT'),
-('62', 'KALIMANTAN TENGAH'),
-('63', 'KALIMANTAN SELATAN'),
-('64', 'KALIMANTAN TIMUR'),
-('65', 'KALIMANTAN UTARA'),
-('71', 'SULAWESI UTARA'),
-('72', 'SULAWESI TENGAH'),
-('73', 'SULAWESI SELATAN'),
-('74', 'SULAWESI TENGGARA'),
-('75', 'GORONTALO'),
-('76', 'SULAWESI BARAT'),
-('81', 'MALUKU'),
-('82', 'MALUKU UTARA'),
-('91', 'PAPUA BARAT'),
-('92', 'PAPUA');
+('36', 'BANTEN');
 
 --
 -- Indexes for dumped tables
