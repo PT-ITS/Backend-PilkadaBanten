91fd5c748ddb1022d8e2223f28e3cf44b78ab848
bagusuntoro <untorobagus65@gmail.com>
2024-10-11 09:51:21 +0700

    update backend

--- app/Models/MasterKabupaten.php
+++ app/Models/MasterKabupaten.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class KabupatenModel extends Model
+class MasterKabupaten extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         "provinsi_id",
         "name",
-        "id_jenis",
     ];
 }
